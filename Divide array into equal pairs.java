3dc1db7d39006cd542f27f933a02cfd6e91bc24d
Ashu Sharma <81765508+ashusharma890@users.noreply.github.com>
2022-05-13 19:12:40 +0530

    Create Divide array into equal pairs.java

---
+++ Divide array into equal pairs.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    public boolean divideArray(int[] nums) {
+        int[] freq = new int[501];
+        for(int num : nums)
+            freq[num]++;
+        
+        for(int num : freq) {
+            if( num == 0 )
+                continue;
+            if( num % 2 != 0 )
+                return false;
+        }
+        return true;
+    }
+}
+
