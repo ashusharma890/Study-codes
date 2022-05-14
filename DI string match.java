50709ce826c0ec3fd2c0374896572a08ff8a5f3c
Ashu Sharma <81765508+ashusharma890@users.noreply.github.com>
2022-05-14 13:44:41 +0530

    Create DI string match.java

---
+++ DI string match.java
@@ -0,0 +1,24 @@
+
+class Solution {
+    public int[] diStringMatch(String s) {
+        int[] arr=new int[s.length()+1];
+        int l=0;
+        int r=s.length();
+        for(int i=0;i<arr.length-1;i++){
+            if(s.charAt(i)=='I')
+            {
+                arr[i]=l;
+                l++;
+            }
+            else
+            {
+                arr[i]=r;
+                r--;
+            }
+        }
+        arr[arr.length-1]=l;
+        return arr;
+    }
+}
+
+
